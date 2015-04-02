b768b5a0d0a19b583bea7b67775a0430b6da7392
Thales Machado <thales.machado@taqtile.com>
2015-04-02 15:59:40 -0300

    refactored start-kafka-shell (it was boring to put the parameters every time

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:0.8.2.0 /bin/bash
+
+host_ip=192.168.59.103:9092
+zk_ip=192.168.59.103:2181
+
+function usage
+{
+  echo
+  echo "Usage: $0 [options]"
+  echo
+  echo "Options:"
+  echo
+  echo "  -h, --help             Output usage information"
+  echo "  -zk, --zookeeper       The zookeeper IP"
+  echo "  -hp, --host-ip         The Host IP"
+  exit 1
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -zk | --zookeeper )           shift
+    zk_ip=$1
+    ;;
+
+    -hp | --host-ip )
+    host_ip=$1
+    ;;
+
+    -h | --help )
+    usage
+    ;;
+
+    * )
+    usage
+
+  esac
+  shift
+done
+
+
+ab -n $n_requests -c $n_c_requests -T 'application/x-www-form-urlencoded' -p post.txt http://localhost:3001/
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$host_ip -e ZK=$zk_ip -i -t wurstmeister/kafka:0.8.2.0 /bin/bash
